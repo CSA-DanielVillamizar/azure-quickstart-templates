bb44d2f5608a7ea5b0d60e374f310d3765c9eb95
Daniel Villamizar <daniel8106@hotmail.com>
2020-10-30 21:19:09 -0500

    Update install_wordpress.sh

--- wordpress-single-vm-ubuntu/install_wordpress.sh
+++ wordpress-single-vm-ubuntu/install_wordpress.sh
@@ -8,8 +8,8 @@ logger "Installing WordPress"
 dbpass=$1
 
 export DEBIAN_FRONTEND=noninteractive
-echo mysql-server-5.6 mysql-server/root_password password $dbpass | debconf-set-selections
-echo mysql-server-5.6 mysql-server/root_password_again password $dbpass | debconf-set-selections
+echo mysql-server-5.7 mysql-server/root_password password $dbpass | debconf-set-selections
+echo mysql-server-5.7 mysql-server/root_password_again password $dbpass | debconf-set-selections
 
 # Install the LAMP stack and WordPress
 apt-get -y install apache2 mysql-server php5 php5-mysql wordpress
